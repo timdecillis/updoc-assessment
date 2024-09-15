9da977eb0efe001cd03aa061ab189fc421e5435b
timdecillis <timdecillis@gmail.com>
2024-09-14 20:40:56 -0700

    completed nested types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,26 +4,13 @@ export type Store = {
 
 export type EmployeeType = {
 	id: string;
-	name: string
-	username: string
-	email: string
-	address: {
-		street: string
-		suite: string
-		city: string
-		zipcode: string
-		geo: {
-			lat: string
-			lng: string
-		};
-	};
-	phone: string
-	website: string
-	company: {
-		name: string
-		catchPhrase: string
-		bs: string
-	};
+	name: string;
+	username: string;
+	email: string;
+	address: AddressType;
+	phone: string;
+	website: string;
+	company: EmployeeCompanyType;
 };
 
 export type CompanyType = {
@@ -31,4 +18,21 @@ export type CompanyType = {
 	employeeCount: number;
 	name: string;
 	employees: EmployeeType[];
+};
+
+type EmployeeCompanyType = {
+	name: string;
+	catchPhrase: string;
+	bs: string;
+};
+
+type AddressType = {
+	street: string;
+	suite: string;
+	city: string;
+	zipcode: string;
+	geo: {
+		lat: string;
+		lng: string;
+	};
 };
