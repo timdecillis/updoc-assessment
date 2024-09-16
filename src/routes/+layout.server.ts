b294bba9a358d07b9af2b9d7e5622b1a68bfba67
timdecillis <timdecillis@gmail.com>
2024-09-15 21:43:41 -0700

    added types to root layout server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,14 @@
+import type { EmployeeType } from '$lib/types';
 import axios from 'axios'
 
 export async function load() {
-  let companiesData = {}
+  const companiesData = {}
 
   const url = 'https://jsonplaceholder.typicode.com/users'
   const response = await axios.get(url)
   const data = response.data;
 
-  data.forEach((employee, i) => {
+  data.forEach((employee: EmployeeType, i: number) => {
     const employeeCompany = employee.company;
     const companyName = employee.company.name;
     if (!companiesData[companyName]) {
