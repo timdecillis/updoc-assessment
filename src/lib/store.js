15f2e68db3148d7e2fef79755ac21b90f153d4e8
timdecillis <timdecillis@gmail.com>
2024-09-15 22:11:22 -0700

    finished store refactor

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,3 +1,44 @@
 import { writable } from 'svelte/store';
 
 export const store = writable({});
+
+// import { writable } from 'svelte/store';
+// import type { EmployeeType, StoreType } from '$lib/types';
+
+// function createCompaniesStore() {
+//     const { subscribe, set, update } = writable<StoreType>({});
+
+//     const fetchCompanies = async () => {
+//         const url = 'https://jsonplaceholder.typicode.com/users';
+//         const response = await fetch(url);
+//         const data = await response.json();
+
+//         const companiesData: StoreType = {};
+
+//         data.forEach((employee: EmployeeType, i: number) => {
+//             const employeeCompany = employee.company;
+//             const companyName = employee.company.name;
+
+//             if (!companiesData[companyName]) {
+//                 companiesData[companyName] = {
+//                     ...employeeCompany,
+//                     employeeCount: 0,
+//                     employees: [],
+//                     id: i
+//                 };
+//             }
+
+//             companiesData[companyName].employeeCount++;
+//             companiesData[companyName].employees.push(employee);
+//         });
+
+//         set(companiesData);
+//     };
+
+//     return {
+//         subscribe,
+//         fetchCompanies
+//     };
+// }
+
+// export const companiesStore = createCompaniesStore();
