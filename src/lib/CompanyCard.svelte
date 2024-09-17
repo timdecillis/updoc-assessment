29f213e918116d9acbfe71b2dedc3d08f9716516
timdecillis <timdecillis@gmail.com>
2024-09-17 15:13:23 -0700

    removed borders

--- src/lib/CompanyCard.svelte
+++ src/lib/CompanyCard.svelte
@@ -5,7 +5,7 @@
   const employeeCount = $store[companyName].employeeCount
 </script>
 
-<div class="bg-gradient-to-br hover:from-green-300 hover:to-blue-400 border border-black bg-slate-200 m-2 p-2 rounded-md">
+<div class="bg-gradient-to-br hover:from-green-300 hover:to-blue-400 border bg-slate-200 m-2 p-2 rounded-md">
   <a class="" href={`employees/${companyName}`}>
     <div class="text-xl">{companyName}</div>
     <div class="font-bold" >"{catchPhrase}"</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div>
-	<div class="p-4 border-b-2 border-slate-700 bg-gradient-to-br from-green-200 to-blue-300">
+	<div class="p-4 border-slate-700 bg-gradient-to-br from-green-200 to-blue-300">
 		<div class="text-5xl">Company Directory</div>
 	</div>
 	<slot />
