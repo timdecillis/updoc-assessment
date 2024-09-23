1cad94ac7f1023f71f1a366efcca3587ddd1c33d
timdecillis <timdecillis@gmail.com>
2024-09-23 14:47:10 -0700

    added link to update on CompanyCard

--- src/routes/employees/[name]/+layout.svelte
+++ src/routes/employees/[name]/+layout.svelte
@@ -3,6 +3,8 @@
 	const companyName = data.companyName;
 </script>
 
-	<div class="text-3xl text-center border-b-2 p-4">{companyName}</div>
-	<slot />
-
+<div class="text-3xl text-center border-b-2 p-4">{companyName}</div>
+<a href={`employees/${companyName}/update`}>
+	<div>Update employees</div>
+</a>
+<slot />
