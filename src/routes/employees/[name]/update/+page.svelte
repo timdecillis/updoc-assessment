d5982580a2c481264e39b5b83216b8841770eed3
timdecillis <timdecillis@gmail.com>
2024-09-27 22:47:29 -0700

    added companies from server

--- src/routes/employees/[name]/update/+page.svelte
+++ src/routes/employees/[name]/update/+page.svelte
@@ -1,1 +1,6 @@
-<div  class="text-center font-bold text-3xl">Update page</div>
+<script>
+	export let data;
+	const companies = data.companies;
+</script>
+
+<div class="text-center font-bold text-3xl">Update page</div>
