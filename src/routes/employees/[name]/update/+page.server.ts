e6e706b4f91defee68154a8af48abbd406526c6c
timdecillis <timdecillis@gmail.com>
2024-09-30 10:37:09 -0700

    reverted upload page

--- src/routes/employees/[name]/update/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import { store } from '$lib/store';
-
-export function load() {
-	return store;
-}
--- src/routes/employees/[name]/update/+page.svelte
+++ src/routes/employees/[name]/update/+page.svelte
@@ -1,9 +1,1 @@
-<script>
-	export let store;
-	$store = store;
-</script>
-
 <div class="text-center font-bold text-3xl">Update page</div>
-<!-- {#each companies as company}
-	<div>Company: {company.name}</div>
-{/each} -->
