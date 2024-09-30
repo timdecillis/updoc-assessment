bae7349b22f1ad2d1698490d10a1ffef046133bc
timdecillis <timdecillis@gmail.com>
2024-09-29 23:06:37 -0700

    added store to upload page

--- src/routes/employees/[name]/update/+page.svelte
+++ src/routes/employees/[name]/update/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	export let data;
-	const companies = data.companies;
+	export let store;
+	$store = store;
 </script>
 
 <div class="text-center font-bold text-3xl">Update page</div>
-{#each companies as company}
+<!-- {#each companies as company}
 	<div>Company: {company.name}</div>
-{/each}
+{/each} -->
