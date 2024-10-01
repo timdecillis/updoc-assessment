a4b7b6db16e20d9c0a96d12ef2db1cb6cb46b6ae
timdecillis <timdecillis@gmail.com>
2024-10-01 13:49:52 -0700

    added company name to update page

--- src/routes/employees/[name]/update/+page.svelte
+++ src/routes/employees/[name]/update/+page.svelte
@@ -1,1 +1,6 @@
-<div class="text-center font-bold text-3xl">Update page</div>
+<script>
+	export let data;
+	const { name } = data;
+</script>
+
+<div class="text-center font-bold text-3xl">{name} Update page</div>
