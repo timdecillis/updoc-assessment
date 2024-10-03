f4a21fa5f49fcc5fd56323d8daf6351a3e04efd4
timdecillis <timdecillis@gmail.com>
2024-10-03 14:28:20 -0700

    added employee id to employee card

--- src/lib/EmployeeCard.svelte
+++ src/lib/EmployeeCard.svelte
@@ -3,7 +3,7 @@
 	import { store } from './store';
 	import type { StoreType } from './types';
 
-	const { name, email, phone } = employee;
+	const { name, email, phone, id } = employee;
 
 	const companies = Object.keys($store).filter((name) => name !== companyName);
 	let selectedCompany: string;
@@ -33,18 +33,20 @@
 	};
 </script>
 
-<div class="p-4 border-black">
-	<div class="text-xl font-bold">{name}</div>
-	<div><span class="font-bold">Email:</span> {email}</div>
-	<div><span class="font-bold">Phone: </span>{phone}</div>
-	<label for="companies">Reassign:</label>
-	<select bind:value={selectedCompany} id="companies" name="options">
-		{#each companies as company}
-			<option selected={companyName === company} value={company}>{company}</option>
-		{/each}
-	</select>
-	<button
-		class="rounded-md p-2 border-2 border-slate-500 hover:bg-slate-500 hover:text-white"
-		on:click={handleReassign}>Reassign</button
-	>
-</div>
+<a href={id} >
+	<div class="p-4 border-black">
+		<div class="text-xl font-bold">{name}</div>
+		<div><span class="font-bold">Email:</span> {email}</div>
+		<div><span class="font-bold">Phone: </span>{phone}</div>
+		<label for="companies">Reassign:</label>
+		<select bind:value={selectedCompany} id="companies" name="options">
+			{#each companies as company}
+				<option selected={companyName === company} value={company}>{company}</option>
+			{/each}
+		</select>
+		<button
+			class="rounded-md p-2 border-2 border-slate-500 hover:bg-slate-500 hover:text-white"
+			on:click={handleReassign}>Reassign</button
+		>
+	</div>
+</a>
