6b0cb75e3ee4915e08ef296dc409f9063560b5e6
timdecillis <timdecillis@gmail.com>
2024-10-03 14:41:21 -0700

    moved reassign into Employee component

--- src/lib/Employee.svelte
+++ src/lib/Employee.svelte
@@ -1,3 +1,38 @@
+<script lang="ts">
+	import { store } from './store';
+	import type { StoreType } from './types';
+	const companies = Object.keys($store).filter((name) => name !== companyName);
+	let selectedCompany: string;
+	const handleReassign = () => {
+		store.update((currentStore: StoreType) => {
+			const formerCompanyEmps = currentStore[companyName]?.employees || [];
+			const reassignedCompanyEmps = currentStore[selectedCompany]?.employees || [];
+
+			const newReassignedCompanyEmps = [...reassignedCompanyEmps, employee];
+
+			const newFormerCompanyEmps = formerCompanyEmps.filter((emp) => emp.name !== employee.name);
+
+			return {
+				...currentStore,
+				[companyName]: {
+					...currentStore[companyName],
+					employees: newFormerCompanyEmps,
+					employeeCount: currentStore[companyName].employeeCount - 1
+				},
+				[selectedCompany]: {
+					...currentStore[selectedCompany],
+					employees: newReassignedCompanyEmps,
+					employeeCount: currentStore[selectedCompany].employeeCount + 1
+				}
+			};
+		});
+	};
+  const employee = 'foobar'
+	const email = 'foo';
+	const phone = 'bar';
+	const companyName = 'foo';
+</script>
+
 <div>Employee Details</div>
 <div><span class="font-bold">Email:</span> {email}</div>
 <div><span class="font-bold">Phone: </span>{phone}</div>
--- src/lib/EmployeeCard.svelte
+++ src/lib/EmployeeCard.svelte
@@ -1,36 +1,7 @@
 <script lang="ts">
 	export let companyName, employee;
-	// import { store } from './store';
-	// import type { StoreType } from './types';
 
 	const { name, id } = employee;
-
-	// const companies = Object.keys($store).filter((name) => name !== companyName);
-	// let selectedCompany: string;
-	// const handleReassign = () => {
-	// 	store.update((currentStore: StoreType) => {
-	// 		const formerCompanyEmps = currentStore[companyName]?.employees || [];
-	// 		const reassignedCompanyEmps = currentStore[selectedCompany]?.employees || [];
-
-	// 		const newReassignedCompanyEmps = [...reassignedCompanyEmps, employee];
-
-	// 		const newFormerCompanyEmps = formerCompanyEmps.filter((emp) => emp.name !== employee.name);
-
-	// 		return {
-	// 			...currentStore,
-	// 			[companyName]: {
-	// 				...currentStore[companyName],
-	// 				employees: newFormerCompanyEmps,
-	// 				employeeCount: currentStore[companyName].employeeCount - 1
-	// 			},
-	// 			[selectedCompany]: {
-	// 				...currentStore[selectedCompany],
-	// 				employees: newReassignedCompanyEmps,
-	// 				employeeCount: currentStore[selectedCompany].employeeCount + 1
-	// 			}
-	// 		};
-	// 	});
-	// };
 </script>
 
 <a href={`${companyName}/${id}`}>
