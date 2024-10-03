390c306f5f898f20b0f4526a607aee65250d3c45
timdecillis <timdecillis@gmail.com>
2024-10-03 14:37:32 -0700

    removed unused code

--- src/lib/EmployeeCard.svelte
+++ src/lib/EmployeeCard.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
 	export let companyName, employee;
-	import { store } from './store';
-	import type { StoreType } from './types';
+	// import { store } from './store';
+	// import type { StoreType } from './types';
 
-	const { name, email, phone, id } = employee;
+	const { name, id } = employee;
 
-	const companies = Object.keys($store).filter((name) => name !== companyName);
-	let selectedCompany: string;
-	const handleReassign = () => {
-		store.update((currentStore: StoreType) => {
-			const formerCompanyEmps = currentStore[companyName]?.employees || [];
-			const reassignedCompanyEmps = currentStore[selectedCompany]?.employees || [];
+	// const companies = Object.keys($store).filter((name) => name !== companyName);
+	// let selectedCompany: string;
+	// const handleReassign = () => {
+	// 	store.update((currentStore: StoreType) => {
+	// 		const formerCompanyEmps = currentStore[companyName]?.employees || [];
+	// 		const reassignedCompanyEmps = currentStore[selectedCompany]?.employees || [];
 
-			const newReassignedCompanyEmps = [...reassignedCompanyEmps, employee];
+	// 		const newReassignedCompanyEmps = [...reassignedCompanyEmps, employee];
 
-			const newFormerCompanyEmps = formerCompanyEmps.filter((emp) => emp.name !== employee.name);
+	// 		const newFormerCompanyEmps = formerCompanyEmps.filter((emp) => emp.name !== employee.name);
 
-			return {
-				...currentStore,
-				[companyName]: {
-					...currentStore[companyName],
-					employees: newFormerCompanyEmps,
-					employeeCount: currentStore[companyName].employeeCount - 1
-				},
-				[selectedCompany]: {
-					...currentStore[selectedCompany],
-					employees: newReassignedCompanyEmps,
-					employeeCount: currentStore[selectedCompany].employeeCount + 1
-				}
-			};
-		});
-	};
+	// 		return {
+	// 			...currentStore,
+	// 			[companyName]: {
+	// 				...currentStore[companyName],
+	// 				employees: newFormerCompanyEmps,
+	// 				employeeCount: currentStore[companyName].employeeCount - 1
+	// 			},
+	// 			[selectedCompany]: {
+	// 				...currentStore[selectedCompany],
+	// 				employees: newReassignedCompanyEmps,
+	// 				employeeCount: currentStore[selectedCompany].employeeCount + 1
+	// 			}
+	// 		};
+	// 	});
+	// };
 </script>
 
 <a href={`${companyName}/${id}`}>
-	<div class="p-4 border-black">
+	<div class="p-4 border-black hover:bg-slate-400">
 		<div class="text-xl font-bold">{name}</div>
 	</div>
 </a>
