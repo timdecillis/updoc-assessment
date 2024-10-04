87c974b27b7768dbc68e71c4dec2c08b4592a87a
timdecillis <timdecillis@gmail.com>
2024-10-04 15:35:37 -0700

    updated routes

--- src/lib/CompanyCard.svelte
+++ src/lib/CompanyCard.svelte
@@ -9,7 +9,7 @@
 <div
 	class="bg-gradient-to-br hover:from-green-300 hover:to-blue-400 border bg-slate-200 m-2 p-2 rounded-md"
 >
-	<a class="" href={`employees/${companyName}`}>
+	<a class="" href={companyName}>
 		<div class="text-xl">{companyName}</div>
 		<div class="font-bold">"{catchPhrase}"</div>
 		<div class="text-sm">Total employees: {employeeCount}</div>
--- src/routes/[name]/+layout.svelte
+++ src/routes/[name]/+layout.svelte
@@ -4,4 +4,5 @@
 </script>
 
 <div class="text-3xl text-center border-b-2 p-4">{companyName}</div>
+<div>Employees</div>
 <slot />
