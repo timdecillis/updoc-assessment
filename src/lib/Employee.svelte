fe4fc2dbb705f3b247b350ca9ad4af573fd0bc81
timdecillis <timdecillis@gmail.com>
2024-10-06 19:51:58 -0700

    added component to employee

--- src/lib/Employee.svelte
+++ src/lib/Employee.svelte
@@ -11,6 +11,8 @@
 		return employee.id === employeeID;
 	})[0];
 
+	const { email, phone } = employee;
+
 	const companiesToSelect = Object.keys($store).filter((name) => name !== company);
 
 	let selectedCompany: string;
@@ -40,16 +42,16 @@
 	};
 </script>
 
-<!-- <div>{employeeID}</div>
+<div>{employeeID}</div>
 <div><span class="font-bold">Email:</span> {email}</div>
 <div><span class="font-bold">Phone: </span>{phone}</div>
 <label for="companies">Reassign:</label>
 <select bind:value={selectedCompany} id="companies" name="options">
-	{#each companiesToSelect as company}
-		<option selected={companyName === company} value={company}>{company}</option>
+	{#each companiesToSelect as companyChoice}
+		<option selected={company === companyChoice} value={companyChoice}>{companyChoice}</option>
 	{/each}
 </select>
 <button
 	class="rounded-md p-2 border-2 border-slate-500 hover:bg-slate-500 hover:text-white"
 	on:click={handleReassign}>Reassign</button
-> -->
+>
