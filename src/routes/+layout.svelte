aa065214f6daaf4453350110b6cd285b54f3d441
timdecillis <timdecillis@gmail.com>
2024-10-07 14:49:00 -0700

    created anchor for header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,10 @@
 </script>
 
 <div>
-	<div class="p-4 border-slate-700 bg-gradient-to-br from-green-200 to-blue-300">
-		<div class="text-5xl">Company Directory</div>
-	</div>
+	<a href="/">
+		<div class="p-4 border-slate-700 bg-gradient-to-br from-green-200 to-blue-300">
+			<div class="text-5xl">Company Directory</div>
+		</div>
+	</a>
 	<slot />
 </div>
